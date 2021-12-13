4cdcbd4d41751d43b1d89d77773025330c6038ab
maestr01 <maestr01@wsc.edu>
2021-12-13 16:33:41 -0600

    Add script to toggle main menu plus start styles

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+const menuBtn = document.getElementById('menuButton');
+const menu = document.getElementById('topMenu');
+
+function closeMenu(){
+  menu.style.display = 'none';
+  menuBtn.disabled = false;
+
+}
+
+function openMenu(){
+  menuBtn.disabled = true;
+  menu.style.display = 'block';
+}
